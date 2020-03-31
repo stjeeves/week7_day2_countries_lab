18a5641233a3ba7e6395ffea7c3758d1c1b94d9c
Stephen <st-jeeves@hotmail.co.uk>
2020-03-31 15:15:47 +0100

    first part of MVP done

---
+++ src/app.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+    new Vue({
+        el: "#app",
+        data: {
+            countries: []
+        },
+        mounted(){
+            this.fetchCountries();
+        },
+        computed: {
+            totalPopulation: function(){
+                return this.countries.reduce((total, country) => total + country.population, 0);
+            },
+
+            findCountry: function () {
+                // return this.countries.find(user input)
+    
+            }
+        },
+        methods: {
+            fetchCountries: function(){
+                fetch('https://restcountries.eu/rest/v2/all')
+                .then( response => response.json())
+                .then( data => this.countries = data)
+                .catch( err => console.error(err))
+            }
+
+            
+        }
+    })
+})
